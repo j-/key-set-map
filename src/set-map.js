94bbee18ac896cc22f1d04d7bb5002a7783e0792
j- <j@skeoh.com>
2016-01-17 22:10:23 +1100

    Add basic documentation

--- src/set-map.js
+++ src/set-map.js
@@ -8,6 +8,11 @@ export default class SetMap {
 		this[MAP] = new Map();
 	}
 
+	/**
+	 * Deletes the value at the given path.
+	 * @param {Array|Set} path Index path
+	 * @return {Boolean} True if a value was deleted, false otherwise.
+	 */
 	delete (path) {
 		const map = this[MAP];
 		const keys = map.keys();
@@ -21,6 +26,11 @@ export default class SetMap {
 		return result;
 	}
 
+	/**
+	 * Delete all items in the map where their paths contain a given value.
+	 * @param {*} item One of the items in the path for each item to delete
+	 * @return {Boolean} True if anything was deleted, false otherwise.
+	 */
 	deleteAll (item) {
 		const map = this[MAP];
 		const keys = map.keys();
@@ -34,6 +44,11 @@ export default class SetMap {
 		return result;
 	}
 
+	/**
+	 * Gets the value at a given path.
+	 * @param {Array|Set} path Index path
+	 * @return {*} The value stored at the given path, or `undefined` if none.
+	 */
 	get (path) {
 		const map = this[MAP];
 		const keys = map.keys();
@@ -45,6 +60,12 @@ export default class SetMap {
 		}
 	}
 
+	/**
+	 * Checks for the existence of an item at the given path.
+	 * @param {Array|Set} path Index path
+	 * @return {Boolean} True if an item is set at the given path, false
+	 *   otherwise.
+	 */
 	has (path) {
 		const map = this[MAP];
 		const keys = map.keys();
@@ -57,6 +78,12 @@ export default class SetMap {
 		return false;
 	}
 
+	/**
+	 * Sets or updates an item at the given path.
+	 * @param {Array|Set} path Index path
+	 * @param {*} value Item to store at the given path
+	 * @return {SetMap} Returns the instance for chaining.
+	 */
 	set (path, value) {
 		const map = this[MAP];
 		const keys = map.keys();
